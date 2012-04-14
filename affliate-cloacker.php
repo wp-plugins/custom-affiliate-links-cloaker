79ca0c132faa34485e64b2f954da61fa7f81d33a
Mousewel <Mousewel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-04-14 11:09:26 +0000

    git-svn-id: https://plugins.svn.wordpress.org/custom-affiliate-links-cloaker/tags/1.4@530965 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- affliate-cloacker.php
+++ affliate-cloacker.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name:   Custom Affiliate Links Cloaker
-Plugin URI: http://wordpress.org
+Plugin URI: http://wordpress.org/extend/plugins/custom-affiliate-links-cloaker/
 Description:  This plugin gathers link information via web service and cloaks affiliate links on page
-Version: 1.2.5
+Version: 1.4
 Author: Ahmad Alinat
-Author URI: http://wordpress.org
+Author URI: http://wordpress.org/extend/plugins/custom-affiliate-links-cloaker/
 License: Free
 */
 
